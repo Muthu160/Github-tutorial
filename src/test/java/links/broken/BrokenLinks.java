b076d364fbf0a74bba96cec703be63d829404dac
Muthu160 <muthu.thala38@gmail.com>
2023-11-22 10:20:18 -0800

    first commit for android testing

---
+++ src/test/java/links/broken/BrokenLinks.java
@@ -0,0 +1,81 @@
+package links.broken;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.time.Duration;
+import java.util.List;
+import java.util.ListIterator;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import org.apache.hc.core5.http.HttpConnection;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BrokenLinks {
+	
+	
+	@Test(enabled = false)
+	public void brokenLinks() throws IOException {
+		
+		
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver = new EdgeDriver();
+		
+		driver.get("https://www.google.co.in/");
+		
+		List<WebElement> allLinks = driver.findElements(By.tagName("a"));
+		
+      int size = allLinks.size();
+      System.out.println("Total links in page " + size);
+		
+		ListIterator<WebElement> listIterator = allLinks.listIterator();
+	;int count = 0;int count1=0;
+		while(listIterator.hasNext()) {
+			
+			WebElement link = listIterator.next();
+			String attribute = link.getAttribute("href");
+			URL u = new URL(attribute);
+			URLConnection openConnection = u.openConnection();
+			HttpsURLConnection hc = (HttpsURLConnection)openConnection;
+			int responseCode = hc.getResponseCode();
+			
+			if (responseCode >=200 && responseCode <=299) {
+				System.out.println("Valid link " + attribute);
+				count++;
+			}
+			else {
+				System.out.println("Invalid Link " + attribute);
+				count1++;
+			}
+		}
+		System.out.println("Valid Links in page  "+count);
+		System.out.println("Invalid Links in page  "+count1);
+		
+		
+		
+		
+		driver.close();
+	}
+
+	
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
---
+++ src/test/java/xpathAxes/XpathAxes.java
@@ -0,0 +1,36 @@
+package xpathAxes;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class XpathAxes {
+	
+	@Test
+	public void xpath() throws InterruptedException {
+		
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver = new EdgeDriver();
+		
+		driver.get("https://www.facebook.com/");
+		WebElement text = driver.findElement(By.xpath("//input[@name=\"email\"]/"
+				+ "parent::div/following-sibling::div/"
+				+ "child::div[@id='passContainer']/ancestor::form/"
+				+ "child::div[@class='_6ltg']/child::button/../"
+				+ "following-sibling::div[@class='_6ltj']/"
+				+ "child::a/../following-sibling::div[@class='_8icz']/"
+				+ "following-sibling::div[@class='_6ltg']/../../../../"
+				+ "preceding-sibling::div/child::h2[@class='_8eso']"));
+		
+		String text2 = text.getText();
+		System.out.println(text2);
+		Thread.sleep(3000);
+		driver.quit();
+		
+
+}
+}
